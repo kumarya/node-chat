e04ba8532ad660e8202f5c9ee1aa929003276734
yakumar <yakumar_in@yahoo.com>
2017-04-23 09:56:41 +0000

    broadcast added

--- server/server.js
+++ server/server.js
@@ -12,22 +12,16 @@ const publicPath = path.join(__dirname, '../public')
 io.on('connection', (socket)=>{
     console.log('new user connected')
     
-    socket.emit('newEmail', {
-        from:'hira@gmail.com',
-        text:"how r u",
-        createdAt:23/22/1980
-    })
-    socket.emit('newMessage', {
-        from:'df@gmail.com',
-        text:'see u',
-        createdAt:233
-    })
-    socket.on('createEmail', function(newEmail){
-        console.log('createEmail', newEmail)
-        
-    })
+    
+    
+    
     socket.on('newMessage', function(newMessage){
         console.log('newMessage', newMessage)
+        io.emit('newMessage', {
+            from:newMessage.from,
+            text:newMessage.text,
+            createdAt: new Date().getTime()
+        })
     })
     
     socket.on('disconnect', ()=>{
